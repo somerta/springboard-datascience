446d8d071c4c6d814509b0617b171e7f078b1ed1
somerta <shirtzel@ybca.org>
2018-05-11 16:54:56 -0700

    .

--- Homework Assignments/1-basic_data_manip.R
+++ Homework Assignments/1-basic_data_manip.R
@@ -1,4 +1,14 @@
 library(readr)
-refine_original <- read_csv("c:/users/shirtzel/documents/gits/springboard-datascience/Homework Assignments/refine_original.csv")
-View(refine_original)
+library(dplyr)
+library(stringr)
 
+refine <- read_csv("c:/users/shirtzel/documents/gits/springboard-datascience/Homework Assignments/refine_original.csv")
+View(refine)
+
+refine %>% 
+  select(company) %>% 
+  group_by(company) %>% 
+  tally()
+
+##not done with this yet. looks like I'm going to need to bust out some mad string identification skillz
+#refine <- read_csv("c:/users/shirtzel/documents/gits/springboard-datascience/Homework Assignments/refine_original.csv")
