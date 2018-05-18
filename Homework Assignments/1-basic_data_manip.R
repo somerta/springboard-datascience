834576afde371e040c804fb32aedba0d5f3afaf4
somerta <shirtzel@ybca.org>
2018-05-18 16:22:11 -0700

    first few projects in the assignment complete!

--- Homework Assignments/1-basic_data_manip.R
+++ Homework Assignments/1-basic_data_manip.R
@@ -5,10 +5,23 @@ library(stringr)
 refine <- read_csv("c:/users/shirtzel/documents/gits/springboard-datascience/Homework Assignments/refine_original.csv")
 View(refine)
 
-refine %>% 
-  select(company) %>% 
-  group_by(company) %>% 
-  tally()
+##List all occurrences of each spelling of each brand name
 
-##not done with this yet. looks like I'm going to need to bust out some mad string identification skillz
-#refine <- read_csv("c:/users/shirtzel/documents/gits/springboard-datascience/Homework Assignments/refine_original.csv")
+# refine %>% 
+#   select(company) %>% 
+#   group_by(company) %>% 
+#   tally()
+
+##replace values to clean up brand names
+
+refine %>% 
+  mutate(company = str_replace(company, regex("^a.*$", ignore_case=TRUE), "Akzo")) %>% 
+  mutate(company = str_replace(company, regex("^(p|f).*$", ignore_case=TRUE), "Philips")) %>%
+  mutate(company = str_replace(company, regex("^u.*$", ignore_case=TRUE), "Unilever")) %>% 
+  mutate(company = str_replace(company, regex("^v.*$", ignore_case=TRUE), "Van Houten")) %>% 
+
+##separate product code and number
+  mutate(product_code = str_match(refine$'Product code / number', ".")) %>% 
+  mutate(product_number = str_match(refine$'Product code / number', "[:digit:]*$")) %>% 
+  print
+       
