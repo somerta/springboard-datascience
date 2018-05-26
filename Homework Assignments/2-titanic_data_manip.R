e5829110a1d72bfd5ad05c449fcf0dd86d39a39c
somerta <shirtzel@ybca.org>
2018-05-25 18:13:39 -0700

    continuing on

--- Homework Assignments/2-titanic_data_manip.R
+++ Homework Assignments/2-titanic_data_manip.R
@@ -2,13 +2,19 @@ library(readr)
 library(dplyr)
 
 set_wd("C:\Users\shirtzel\Documents\gits\springboard-datascience\Homework Assignments")
-
 titan <- read.csv("titanic_original.csv")
 ##View(titan)
 ##str(titan)
 
+#edit columns: embarked: convert all blank cells to S // age: convert all NAs to the mean population age
+
 titan %>% 
   mutate(embarked = ifelse(embarked == "", "S", as.character(embarked))) %>% 
+  mutate(age = ifelse(is.na(age), 29.88113, age)) %>% 
   select_all %>% 
   arrange(embarked)
-  
+
+#find the mean age
+mean(titan$age, na.rm=TRUE) ## 29.88113
+
+
